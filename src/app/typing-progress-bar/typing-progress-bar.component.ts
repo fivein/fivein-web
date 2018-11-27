bd2d29176e380a4b89e541f2befe2eb7bd4a4620
plty <platyunyu@gmail.com>
2018-11-28 06:14:55 +0800

    add working percentage progress bar

--- src/app/typing-progress-bar/typing-progress-bar.component.ts
+++ src/app/typing-progress-bar/typing-progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-typing-progress-bar',
@@ -6,11 +6,25 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./typing-progress-bar.component.sass']
 })
 export class TypingProgressBarComponent implements OnInit {
-  @Input() value: Number;
-  @Input() prediction: Number;
+  @ViewChild('badgeContainer') badgeContainer: ElementRef;
+  @ViewChild('filler') filler: ElementRef;
+  @ViewChild('badge') badge: ElementRef;
+  @Input() value: number;
+  @Input() name: String;
+  trackLength: number;
+  badgeWidth: number;
+  fillerLength: number;
 
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.trackLength = this.badgeContainer.nativeElement.offsetWidth;
+    this.badgeWidth = this.badge.nativeElement.offsetWidth;
+    // this.fillerLength = (this.trackLength - this.badgeWidth); //(this.trackLength - this.badgeWidth)// * this.value / 100.0 as Number;
+    this.fillerLength = (this.trackLength - this.badgeWidth) * this.value / 100.0;
+    this.filler.nativeElement.style.width = String(this.fillerLength) + 'px';
+    console.log(this.trackLength, this.badgeWidth, this.fillerLength, this.value);
   }
 }
