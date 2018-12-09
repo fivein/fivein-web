f729bc109d23d80db4c61dc330fb1a0f2c7e9337
plty <platyunyu@gmail.com>
2018-12-10 00:17:15 +0800

    rebuilt progress bar

---
+++ src/app/passage/passage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-passage',
+  templateUrl: './passage.component.html',
+  styleUrls: ['./passage.component.sass']
+})
+export class PassageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
